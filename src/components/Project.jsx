98857945619162d1298c1800ddd1a79d70ebfeaa
Abderrezague ismail <ki_abderrezague@esi.dz>
2025-08-05 02:00:20 +0100

    fixing sections

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -29,9 +29,9 @@ const Project = ({image,title,techs,gitHub,demo}) => {
         }
         </div>
       <div className='absolute bottom-4 left-0 flex gap-6'>
-        <Link href={`${gitHub}`} target='_blank' className='text-white hover:text-blue-700'>
+        {gitHub && <Link href={`${gitHub}`} target='_blank' className='text-white hover:text-blue-700'>
           <FaGithub size={20}/>
-        </Link>
+        </Link>}
         {demo && <Link href={`${demo}`} target='_blank' className='text-white hover:text-blue-700'>
           <FaExternalLinkAlt size={20}/>
         </Link>}
